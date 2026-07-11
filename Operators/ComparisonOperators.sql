bf3d86b3b659d764d40d133e7eaf2d2aa734d31b
sravyamalyala7 <sravyamalyala7@gmail.com>
2026-07-11 15:13:01 +0530

    Add comparison operators queries

---
+++ Operators/ComparisonOperators.sql
@@ -0,0 +1,16 @@
+-- Comparison Operators
+
+SELECT * FROM Student
+WHERE Age > 20;
+
+SELECT * FROM Student
+WHERE Age < 20;
+
+SELECT * FROM Student
+WHERE Age >= 21;
+
+SELECT * FROM Student
+WHERE Age <= 21;
+
+SELECT * FROM Student
+WHERE Age != 21;
